963a7f8d107bb8700ece5d17dbf45dac8f9197d4
Moishe Lettvin <moishel@gmail.com>
2017-10-26 11:54:51 -0600

    Now we are listening for RTM events

--- gentle_reminder.js
+++ gentle_reminder.js
@@ -33,4 +33,65 @@ app.listen(http_port, function(err) {
   }
 
   console.log('Listening on ' + http_port);
+
+  var gentleReminder = new GentleReminder();
+
+  gentleReminder.init(slackClient, slackToken);
+  gentleReminder.start();
 });
+
+function GentleReminder() {
+  console.log("constructing.");
+
+  this.slackClient = undefined;
+
+  this.token = undefined;
+
+  this.rtm = undefined;
+
+  this.commonStorage = undefined;
+  this.userStorage = undefined;
+  this.moduleStorage = undefined;
+
+  this.DEFAULT_MODULE_NAME = 'default';
+
+  console.log("constructed.");
+}
+
+GentleReminder.prototype.init = function(slackClient, token){
+  console.log("initializing.");
+  this.slackClient = slackClient;
+  this.token = token;
+
+  this.rtm = new this.slackClient.RtmClient(this.token, { logLevel: 'warning' });
+
+  this.rtm.on(this.slackClient.CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData) => {
+    for (const c of rtmStartData.channels) {
+      if (c.is_member && c.name ==='general') { channel = c.id }
+    }
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+  });
+
+  this.channelRe = /#.*/;
+  this.userRe = /<@[UW][A-Za-z0-9]+>/;
+
+  console.log("initialized.");
+};
+
+GentleReminder.prototype.start = function() {
+  console.log("starting");
+  this.rtm.start();
+
+  var self = this;
+  this.rtm.on(this.slackClient.RTM_EVENTS.MESSAGE, function(m) {
+    console.log(m);
+  });
+  this.rtm.on(this.slackClient.RTM_EVENTS.REACTION_ADDED, function handleRtmReactionAdded(reaction) {
+    // TODO
+  });
+
+  this.rtm.on(this.slackClient.RTM_EVENTS.REACTION_REMOVED, function handleRtmReactionRemoved(reaction) {
+    // TODO
+  });
+  console.log("started");
+};
