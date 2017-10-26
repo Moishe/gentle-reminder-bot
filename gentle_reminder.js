30eddf986a80b9de2f349b2e2965a233b6bf3f1f
Moishe Lettvin <moishel@gmail.com>
2017-10-26 11:28:02 -0600

    Bare bones code for bot; adding some dependencies

---
+++ gentle_reminder.js
@@ -0,0 +1,36 @@
+// In the future we should serve a pretty webpage with user stats!
+// For now we have a simple app that doesn't do anything but listen
+// and serve a dummy webpage
+
+const express = require('express');
+const proxy = require('express-http-proxy');
+const bodyParser = require('body-parser');
+const _ = require('lodash');
+
+var http_port = process.env.PORT || '8080';
+var bot_name = process.env.BOT_NAME ||'catbot';
+var slackToken = process.env.SLACK_API_TOKEN;
+var slackClient = require('@slack/client');
+
+var app = express();
+
+if (process.env.PROXY_URI) {
+  app.use(process.env.PROXY_URI), {
+    forwardPath: function(req, res) { return require('url').parse(req.url).path }
+  }
+}
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) { res.send('\n 😻😻 ' + bot_name + ' 😻😻 \n') });
+app.use(express.static(__dirname + '/assets'));
+
+
+app.listen(http_port, function(err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('Listening on ' + http_port);
+});
