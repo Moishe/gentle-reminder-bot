61707a06228e63cae123d2af677e990abb2015f1
Moishe Lettvin <moishel@gmail.com>
2017-10-26 18:17:59 -0600

    Debugging

--- gentle_reminder.js
+++ gentle_reminder.js
@@ -164,7 +164,8 @@ GentleReminder.prototype.start = function() {
 };
 
 GentleReminder.prototype.replace = function(payload) {
-  if (!payload.actions[0].value){
+  console.log(JSON.stringify(payload.actions[0]));
+  if (payload.actions[0].name != 'ignore'){
     return "Okay, ignored.";
   }
   this.web_user.chat.update(payload.callback_id, payload.channel.id, payload.actions[0].value, {}, (err, info) => {
