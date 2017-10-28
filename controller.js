2b20f99ec04d1a87ccbff995d21dc6c1a75440e1
Moishe Lettvin <moishel@gmail.com>
2017-10-28 15:53:12 -0600

    Fixing another dumb error

--- controller.js
+++ controller.js
@@ -44,12 +44,12 @@ Controller.prototype.replace = function(payload) {
     return "Okay, ignored.";
   }
 
-  [team_id, user_id, ts] = payload.callback_id.split("-");
+  var [team_id, user_id, ts] = payload.callback_id.split("-");
 
   console.log(sprintf("Would replace with '%s' on team %s, user %s and ts %s", payload.actions[0].value, team_id, user_id, ts));
 
   if (team_id in this.teamControllers){
-    this.teamControllers[team_id].replace(user, payload.channel.id, ts, payload.actions[0].value);
+    this.teamControllers[team_id].replace(user_id, payload.channel.id, ts, payload.actions[0].value);
   }else{
     console.log('Team not found in team controllers');
   }
