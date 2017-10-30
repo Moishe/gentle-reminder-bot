26cf16fdfea5254e7d50e6c34827e0d046fbb161
Moishe Lettvin <moishel@gmail.com>
2017-10-30 17:44:22 -0600

    fix error

--- gentle_reminder.js
+++ gentle_reminder.js
@@ -33,7 +33,7 @@ var controller = new Controller.Controller();
 var db = new DB.DB();
 
 app.post('/interactive', function(req, res) {
-    console.log('payload', payload);
+    console.log('payload', req.body.payload);
     payload = JSON.parse(req.body.payload);
     response = controller.replace(payload);
     res.send(response);
