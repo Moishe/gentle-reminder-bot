fc7968ba8d0d6fcf5dfeccb8de7061b754a3629b
Moishe Lettvin <moishel@gmail.com>
2017-11-06 13:21:55 -0700

    Remove a console.log; clarify some help text

--- team_controller.js
+++ team_controller.js
@@ -115,8 +115,6 @@ TeamController.prototype.handleMessage = function(m) {
         return;
     }
 
-    console.log(m);
-
     if (m.channel[0] == 'D'){
         return this.handleDirectMessage(m);
     }
@@ -204,8 +202,8 @@ TeamController.prototype.handleOAuthCallback = function(user, code, state, redir
 };
 
 TeamController.prototype.showHelp = function(m, args) {
-    var helptext = "Welcome to the Gentle Reminder bot. You can send this bot a message via DM, or by using the `/gentlereminder` slash command.\n\n";
-    helptext += "Be sure to invite the bot (with `invite @gentle-reminder`) to any channels in which you'd like reminders.";
+    var helptext = "Welcome to the Gentle Reminder bot. You can send this bot a message via DM, or by using the `/gentlereminder` slash command.\n";
+    helptext += "Be sure to invite the bot (with `/invite @gentle-reminder`) to any channels in which you'd like reminders.\n\n";
     helptext += "*Commands:*\n";
 
     for (let command in this.commands) {
